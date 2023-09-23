209839a4127b20c73539047e2fbf50dd05d7b681
Laique Merlin Djeutchouang <ldjeutchouang@dtn.chpc.ac.za>
2023-09-23 14:09:21 +0200

    Code refactoring and optimization

--- age_made_simple/age_calculator.py
+++ age_made_simple/age_calculator.py
@@ -4,39 +4,34 @@ class AgeCalculator:
     """
     This program calculates a person's age based on their date of birth and return the day of the week they were born. 
     """
-    def __init__(self, date_of_birth: str) -> None:
-        """
-        Args:
-            date_of_birth (str): use this format YYYY-MM-DD.
-        """
-        try:
-            # Convert the input string to a datetime object
-            self.date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d')
-        except ValueError:
-            print("Invalid date of birth format. Please use this format: 'YYYY-MM-DD'.")
-
-    def calculate_age(self) -> int:
+    def calculate_age(self, date_of_birth: str) -> int:
         """
         Calculate the age based on the provided date of birth.
         Returns:
             int: the calculated age.
         """
+        # Validate the provided date
+        formatted_date = date_format_validator(date_of_birth)
+        
         # Get the current date
         current_date = datetime.now()
         
         # Calculate the age
-        age = current_date.year - self.date_of_birth.year - ((current_date.month, current_date.day) < (self.date_of_birth.month, self.date_of_birth.day))
+        age = current_date.year - formatted_date.year - ((current_date.month, current_date.day) < (formatted_date.month, formatted_date.day))
         
         return age
 
-    def day_of_week(self) -> str:
+    def day_of_week(self, date_of_birth: str) -> str:
         """
         Determine the day of the week the person was born based on the provided date of birth.
         Returns:
             str: the day of the week the person was born.
         """
+        # Validate the provided date
+        formatted_date = date_format_validator(date_of_birth)
+        
         # Get the day of the week as an integer (0 = Monday, 6 = Sunday)
-        day_index = self.date_of_birth.weekday()
+        day_index = formatted_date.weekday()
 
         # Define a list of days of the week
         days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -46,33 +41,41 @@ class AgeCalculator:
 
         return day_name
     
-    def get_birth_details(self) -> None:
+    def get_birth_details(self, date_of_birth: str) -> None:
         """
         Display both the calculated age based on the provided date of birth and return the day of the week the person was born.
         """
         # Calculate age and day of the week
-        age = self.calculate_age()
-        day_name = self.day_of_week()
+        age = self.calculate_age(date_of_birth)
+        day_name = self.day_of_week(date_of_birth)
 
         print(f"\nYou are {age} years old and you were born on a {day_name}.\n\n")
+        
+
+def date_format_validator(date: str):
+    """
+    Args:
+        date (str): use this format YYYY-MM-DD.
+    """
+    try:
+        # Convert the input string to a datetime object
+        return datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        print("\nInvalid date of birth format. Please use this format: 'YYYY-MM-DD'.\n")
 
 if __name__ == "__main__":
     # Input date of birth in the format 'YYYY-MM-DD'
-    date_of_birth_str = input("Enter your date of birth (YYYY-MM-DD): ")
+    date_of_birth_str = input("\nEnter your date of birth (YYYY-MM-DD): ")
 
-    try:
-        # Convert the input string to a datetime object
-        date_of_birth = datetime.strptime(date_of_birth_str, '%Y-%m-%d')
+    # Create an instance of the AgeCalculator class
+    calculator = AgeCalculator()
 
-        # Create an instance of the AgeCalculator class
-        calculator = AgeCalculator(date_of_birth)
+    # Calculate age and day of the week
+    age = calculator.calculate_age(date_of_birth_str)
+    day_name = calculator.day_of_week(date_of_birth_str)
 
-        # Calculate age and day of the week
-        age = calculator.calculate_age()
-        day_name = calculator.day_of_week()
-
-        print(f"You are {age} years old.")
-        print(f"You were born on a {day_name}.")
-    except ValueError:
-        print("Invalid date of birth format. Please use 'YYYY-MM-DD'.")
-
+    print(f"\nYou are {age} years old.")
+    print(f"You were born on a {day_name}.")
+    
+    # Display age and day of the week at once
+    calculator.get_birth_details(date_of_birth_str)
